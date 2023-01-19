81b5f7c56aed38d180e0b6b745cde976022ddaba
anamacam <anamacam.am@gmail.com>
2023-01-19 08:13:45 -0500

    first commit

---
+++ src/main/java/com/example/persistencia/java/PersistenciaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.persistencia.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaJavaApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(PersistenciaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/persistencia/java/PersistenciaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.persistencia.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenciaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
